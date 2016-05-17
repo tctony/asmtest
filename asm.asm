cb5128aa62c997ba0e6b1d3d535f95187402e9ff
Tony Tang <tangchang21@gmail.com>
2016-05-17 16:53:36 +0800

    asm hello

--- asm.asm
+++ asm.asm
@@ -1,15 +1,19 @@
-  default rel
+default rel
 
-  segment .data
 
-  segment .bss
+segment .data
+global _asm_hello
 
-  segment .text
-  global _asm_1024, _asm_read_int
+_asm_hello:
+  db "hello asm", 0
+
+
+segment .bss
+
+
+segment .text
+global _asm_1024
 
 _asm_1024:
   mov rax, 1024
   ret
-
-_asm_read_int:
-
--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
 extern long long asm_1024();
+extern char asm_hello[9];
 
 int main(int argc, char *argv[])
 {
   long long ret = asm_1024();
   printf("%lld\n", ret);
+
+  printf("%s\n", asm_hello);
+
   return 0;
 }
